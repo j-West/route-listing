fca40b90891d175841ed3d8f2cf0b858d082fd1d
j-west <westja89@gmail.com>
2017-01-16 14:39:57 -0600

    Add controller for the second featured highway

---
+++ controllers/SecondHwCtrl.js
@@ -0,0 +1,9 @@
+app.controller('SecondHwCtrl', function ($scope) {
+  console.log("list controller!")
+  $scope.highwayName = "I-65";
+  $scope.highwayDescription = "Interstate 65 (I-65) is a major Interstate Highway in the central United States. As with most interstates that end in a five, it is a major cross-country, north-south route, connecting the Gulf of Mexico to the Great Lakes. Its southern terminus is located at an interchange with I-10 in Mobile, Alabama, and its northern terminus is at an interchange with I-90, U.S. Route 12 (US 12), and US 20 (the Dunes Highway) in Gary, Indiana, just southeast of Chicago.";
+
+
+
+
+})
