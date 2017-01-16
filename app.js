ef4777549dabe8cfaac1744a6816d2c0c8886fcb
j-west <westja89@gmail.com>
2017-01-16 14:40:28 -0600

    Add app.js with config function for routes

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("!");
+
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(($routeProvider, $locationProvider) => {
+  $locationProvider.hashPrefix('');
+  $routeProvider
+    .when("/", {
+      controller : "MainCtrl",
+      templateUrl: "partials/main.html",
+    })
+    .when("/firsthw", {
+      controller: "FirstHwCtrl",
+      templateUrl: "partials/firsthw.html",
+    })
+    .when("/secondhw", {
+      controller: "SecondHwCtrl",
+      templateUrl: "partials/secondhw.html",
+    })
+})
